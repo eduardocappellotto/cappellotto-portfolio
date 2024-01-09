061d87ae17130c05ae833f49b709432222e747cf
Eduardo <eduardocappellotto@gmail.com>
2024-01-09 00:11:09 -0300

    feat: added title to the page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Eduardo Cerioni Cappellotto</title>
+      </Head>
       <body className="bg-primary-dark w-3/4 m-auto">
         <Main />
         <NextScript />
