9e9d1e723818c30360f7b1c16b5ccfcfcd025c10
root <root@CappellottoW11>
2025-01-08 23:30:00 -0300

    fix: url da foto de perfil

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
   return (
     <header className="p-4 flex flex-col lg:flex-row justify-between items-center">
       <img
-        src="https://media.licdn.com/dms/image/D4D03AQE_7j22GVeZUg/profile-displayphoto-shrink_800_800/0/1664902432478?e=1710374400&v=beta&t=8UPP2H-Pba_CwU3JApJPw7aGyLt-byMcc3ZSkZfRMFE"
+        src="https://media.licdn.com/dms/image/v2/D4D03AQHXhrznD6LrMA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1710283000443?e=1741824000&v=beta&t=F7LWxUB5LvJTu_Gw8ZisLy7VVyxnMivtcmSh5qW4Nl8"
         alt="Your Photo"
         className="h-36 rounded-full mb-4 lg:mb-0"
       />
