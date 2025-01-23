7eba011c1af01c06f3e5a19a39975e60da215793
Eduardo Cappellotto <eduardocappellotto@gmal.com>
2025-01-23 17:07:28 -0300

    fix: years from 2019

--- src/components/ExperienceSection.tsx
+++ src/components/ExperienceSection.tsx
@@ -1,14 +1,17 @@
-// components/ExperienceSection.tsx
 import React from "react";
 import Card from "./Card";
 
 const ExperienceSection: React.FC = () => {
+  const startYear = 2019;
+  const currentYear = new Date().getFullYear();
+  const yearsOfExperience = currentYear - startYear;
+
   return (
     <section className="my-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
       <div className="col-span-1 lg:col-span-1">
         <Card style={{ backgroundColor: "primary" }}>
           <div className="flex flex-wrap">
-            <h2 className="text-6xl font-bold self-start ml-2 mt-1">4+</h2>
+            <h2 className="text-6xl font-bold self-start ml-2 mt-1">{yearsOfExperience}+</h2>
             <div className="ml-4">
               <h3 className="text-xl font-semibold">
                 Years in software development
